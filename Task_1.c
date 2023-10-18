a8f78371a6f67177632f4bf2159366ae7095e11c
Mujtaba Ahmad <Mujtaba@Mujtabas-MacBook-Air.local>
2023-10-18 10:54:22 +0500

    first

---
+++ Task_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int dig1, dig2, dig3, check, dig;
+    printf("Give a 3-digit number : ");
+    scanf("%1d%1d%1d",&dig1,&dig2,&dig3);
+    check = pow(dig1,3)+pow(dig2,3)+pow(dig3,3);
+    dig = (dig1*100) + (dig2*10) + dig3;
+    if (check == dig )
+        printf("The number is armstrong number");
+    else
+        printf("The number is not and armstrong number");
+}
